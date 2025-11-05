790c71a516808ef284fe7b5b6d822c2d905d6c9e
RomainRy <romain.royer78@gmail.com>
2025-11-05 11:09:48 +0100

    Initial Commit

---
+++ CountryApp/CountryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryAppApp.swift
+//  CountryApp
+//
+//  Created by ROYER Romain on 05/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CountryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CountryAppTests/CountryAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountryAppTests.swift
+//  CountryAppTests
+//
+//  Created by ROYER Romain on 05/11/2025.
+//
+
+import Testing
+@testable import CountryApp
+
+struct CountryAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
